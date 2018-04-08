b4a3b9b6d5d05ddbbedaadf11f4aa288de6b4ab3
kirkpe <kirkperkins99@gmail.com>
2018-04-08 12:29:32 -0700

    Added Spawn and Pickups

--- TAFAN/Assets/Pickup.cs
+++ TAFAN/Assets/Pickup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour {
-
+    public Scoulville_Manager Score;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,7 +18,8 @@ public class Pickup : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
-
+            Score.Scoulville += 10;
+            Destroy(this.gameObject);
         }
     }
 }
---
+++ TAFAN/Assets/Scoulville_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoulville_Manager : MonoBehaviour {
+    public int Scoulville;
+    public int Health=100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
